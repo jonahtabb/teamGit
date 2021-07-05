efe3d1d93c26ffcf75f1d4ba31b30268f5148c65
Hannah <hannahtoth@Hannahs-MacBook-Pro-2.local>
2021-07-05 18:47:00 -0400

    cleaning the code

--- 72-hour-project/src/components/WeatherComp.js
+++ 72-hour-project/src/components/WeatherComp.js
@@ -3,12 +3,13 @@ import React, {useState} from "react";
 
 const WeatherComp = (props) => {
  //const apiKey = 'a68142f16f5a91861dee80d5b3842040'
- const baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=metric&appid=a68142f16f5a91861dee80d5b3842040`;
+ const baseURL = `https://api.openweathermap.org/data/2.5/weather?lat=${props.latitude}&lon=${props.longitude}&units=imperial&appid=a68142f16f5a91861dee80d5b3842040`;
 const testUrl = `api.openweathermap.org/data/2.5/weather?zip=46201,us&callback=test&appid=a68142f16f5a91861dee80d5b3842040`
  
-    const [mainTemp,setMainTemp] = useState('');
+    const [mainTemp,setMainTemp] = useState('')
     const [description,setDescription] = useState('');
     const [name, setName] = useState('')
+   
 
    
  
@@ -21,6 +22,7 @@ const fetchData = () => {
         setName (weatherData.name);
         setMainTemp (Math.round(weatherData.main.temp));
         setDescription(weatherData.weather[0].description);
+       
         console.log(weatherData)
 })
 
@@ -31,10 +33,13 @@ return(
     <div>
         <h2>WeatherMap Api</h2>
         <button onClick={fetchData}>Click for Weather in your Area</button>
-        <h3>City: {name} </h3>
-        <h4>Tempature (Celsius): {mainTemp}</h4>
-        <h4>Tempature (Fahrenheit): {mainTemp * 1.8 + 32}</h4>
-        <h4>Looks like: {description}</h4>
+        
+     
+        <h3>Neighborhood: {name} </h3>
+        <p>Tempature: {mainTemp} °F</p>
+        <p>Looks like: {description} </p>
+   
+
     </div>
 )}
 
